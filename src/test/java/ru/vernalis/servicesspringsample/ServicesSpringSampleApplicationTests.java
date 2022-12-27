aeffaee7961d4f56143f50afb31d6d0955162840
Ivan Sheckoldin <darthmantis@yandex.ru>
2022-12-27 18:56:33 +0300

    update config (#1)
    remove encoder

--- src/test/java/ru/vernalis/servicesspringsample/ServicesSpringSampleApplicationTests.java
+++ src/test/java/ru/vernalis/servicesspringsample/ServicesSpringSampleApplicationTests.java
@@ -2,6 +2,7 @@ package ru.vernalis.servicesspringsample;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
